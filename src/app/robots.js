1e80b9f23f27365fb4c3efbe43f9aa0d946782a5
Mncedisi <mncedicy@gmail.com>
2026-08-11 08:14:34 +0200

    sitemap

---
+++ src/app/robots.js
@@ -0,0 +1,18 @@
+// /src/app/robots.js
+
+export default function robots() {
+    const baseDomainUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://ecoroute.stims.co.za';
+
+    return {
+        rules: {
+            userAgent: '*',
+            allow: '/',
+            disallow: [
+                '/api/',      // Blocks backend routes from showing up on public search results pages
+                '/*?token=',  // Blocks secure login token links
+                '/*?userId='
+            ],
+        },
+        sitemap: `${baseDomainUrl}/sitemap.xml`,
+    };
+}
